1bf7d8484b3e704af5b457650923902d9a4e8460
SRIJAN23MISHRA <srijananshul@gmail.com>
2026-09-26 11:19:25 +0000

    add Topic and Note models

---
+++ backend/models/Topic.js
@@ -0,0 +1,14 @@
+const mongoose=require("mongoose");
+const TopicSchema=new mongoose.Schema({
+        name:{
+            type:String,
+            required:true
+        },
+        user:{
+            type:mongoose.Schema.Types.ObjectId,
+            ref:"User",
+            required:true
+        },
+
+},{timestamps:true})
+module.exports=mongoose.model("Topic",TopicSchema);
